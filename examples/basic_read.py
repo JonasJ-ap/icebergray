73f508c3525060891717231ada80302b58e2cd08
Rushan Jiang <rushanj@andrew.cmu.edu>
2023-04-02 21:04:24 -0400

    working demo by applying solution in https://github.com/ray-project/ray/issues/21957

--- examples/basic_read.py
+++ examples/basic_read.py
@@ -3,11 +3,21 @@ from pyiceberg.catalog import load_catalog
 
 DB_NAME = "iceberg_ref"
 
-TEST_TABLE_NAMES = ["iceberg_ref.nyc_taxis", "iceberg_ref.iceberg_all_types_parquet", "iceberg_ref.iceberg_nested_parquet"]
+TEST_TABLE_NAMES = ["iceberg_ref.nyc_taxis",
+                    "iceberg_ref.iceberg_all_types_parquet",
+                    "iceberg_ref.iceberg_nested_parquet",
+                    "iceberg_ref.iceberg_all_primitive_types_id_bool_partitioned_parquet",
+                    "iceberg_ref.iceberg_all_primitive_types_id_bool_partitioned_parquet",
+                    "iceberg_ref.iceberg_all_primitive_types_id_partitioned_parquet",
+                    "iceberg_ref.iceberg_all_types_id_partitioned_parquet",
+                    "iceberg_ref.iceberg_all_types_partitioned_parquet_v1",
+                    "iceberg_ref.iceberg_nested_partitioned_parquet",
+                    "iceberg_ref.iceberg_nested_partitioned_parquet_v1"
+                    ]
 
 catalog = load_catalog("default")
 
 if __name__ == "__main__":
-    for table_name in ["iceberg_ref.iceberg_all_types_partitioned_parquet_v1"]:
+    for table_name in TEST_TABLE_NAMES:
         print(f"Reading table {table_name}")
         print(read_iceberg(table_name, catalog_name="default").limit(100).to_pandas(limit=100))
--- icebergray/read_api.py
+++ icebergray/read_api.py
@@ -13,18 +13,18 @@ from ray.data.datasource import DefaultParquetMetadataProvider
 
 
 def read_iceberg(
-    table_identifier: str,
-    *,
-    catalog_name: Optional[str] = None,
-    snapshot_id: Optional[int] = None,
-    case_sensitive: bool = True,
-    filesystem: Optional[FileSystem] = None,
-    columns: Optional[List[str]] = None,
-    parallelism: int = -1,
-    ray_remote_args: Dict[str, Any] = None,
-    tensor_column_schema: Optional[Dict[str, Tuple[np.dtype, Tuple[int, ...]]]] = None,
-    meta_provider=DefaultParquetMetadataProvider(),
-    **arrow_parquet_args,
+        table_identifier: str,
+        *,
+        catalog_name: Optional[str] = None,
+        snapshot_id: Optional[int] = None,
+        case_sensitive: bool = True,
+        filesystem: Optional[FileSystem] = None,
+        columns: Optional[List[str]] = None,
+        parallelism: int = -1,
+        ray_remote_args: Dict[str, Any] = None,
+        tensor_column_schema: Optional[Dict[str, Tuple[np.dtype, Tuple[int, ...]]]] = None,
+        meta_provider=DefaultParquetMetadataProvider(),
+        **arrow_parquet_args,
 ):
     # TODO: in pyiceberg0.4.0, we can have a default-catalog option: https://github.com/apache/iceberg/pull/6864
     catalog = load_catalog(catalog_name) if catalog_name else load_catalog()
@@ -43,19 +43,18 @@ def read_iceberg(
     )
 
 
-
 def _read_iceberg(
-    table: Table,
-    *,
-    snapshot_id: Optional[int] = None,
-    case_sensitive: bool = True,
-    filesystem: Optional[FileSystem] = None,
-    columns: Optional[List[str]] = None,
-    parallelism: int = -1,
-    ray_remote_args: Dict[str, Any] = None,
-    tensor_column_schema: Optional[Dict[str, Tuple[np.dtype, Tuple[int, ...]]]] = None,
-    meta_provider=DefaultParquetMetadataProvider(),
-    **arrow_parquet_args,
+        table: Table,
+        *,
+        snapshot_id: Optional[int] = None,
+        case_sensitive: bool = True,
+        filesystem: Optional[FileSystem] = None,
+        columns: Optional[List[str]] = None,
+        parallelism: int = -1,
+        ray_remote_args: Dict[str, Any] = None,
+        tensor_column_schema: Optional[Dict[str, Tuple[np.dtype, Tuple[int, ...]]]] = None,
+        meta_provider=DefaultParquetMetadataProvider(),
+        **arrow_parquet_args,
 ) -> Dataset:
     table_scan = table.scan(snapshot_id=snapshot_id, case_sensitive=case_sensitive)
     planned_files = [file_task.file.file_path for file_task in table_scan.plan_files()]
@@ -70,5 +69,6 @@ def _read_iceberg(
         ray_remote_args=ray_remote_args,
         tensor_column_schema=tensor_column_schema,
         meta_provider=meta_provider,
+        dataset_kwargs=dict(partitioning=None),  # required since https://github.com/ray-project/ray/issues/21957
         **arrow_parquet_args,
     )
